8cbeffb6ecf1e96a1ad19de2c8d6628a0e8c6b53
Anna Drobysh <ann.drobysh@yandex.by>
2021-08-15 20:32:35 +0200

    Add "Sign up new account" test

--- fixture/Application.py
+++ fixture/Application.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.Project import ProjectHelper
+from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 class Application:
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -15,7 +18,11 @@ class Application:
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
-        self.base_url = base_url
+        self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
+        self.config = config
+        self.base_url = config['web']['baseUrl']
 
     def is_valid(self):
         try:
